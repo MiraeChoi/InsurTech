09d3657abed341875847c5fcb6c627246b5d2509
Lee-Soyeon <leesoyena@gmail.com>
2020-10-26 15:53:47 +0900

    메인 화면

--- express.js
+++ express.js
@@ -5,11 +5,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
 
+app.get('/index',function(req,res){
+    res.render('index');
+});
 app.get('/login',function(req,res){
     res.render('login');
-})
+});
 app.get('/register',function(req,res){
     res.render('register');
-})
+});
 
 app.listen(3000);
