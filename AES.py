10bb4bb91f81eff110adf9fa77e7d00a51fdf1f2
eeees <qmffkselsk@naver.com>
2020-10-27 16:52:07 +0900

    frontend

---
+++ AES.py
@@ -0,0 +1,54 @@
+﻿# -*- coding: utf-8 -*-
+import base64
+from Crypto.Cipher import AES
+
+#AES 암호화 클래스 (AES-CBC-PKC7padding)
+class AESCipher:
+	class InvalidBlockSizeError(Exception):
+		"""Raised for invalid block sizes"""
+		pass
+
+	def __init__(self, key, iv):
+		self.key = key
+		self.iv =iv
+
+	def __pad(self, text):
+		text_length = len(text)
+		amount_to_pad = AES.block_size - (text_length % AES.block_size)
+		if amount_to_pad == 0:
+			amount_to_pad = AES.block_size
+		pad = chr(amount_to_pad)
+
+		result = None
+		try:
+			result = text + str(pad * amount_to_pad).encode("UTF-8")
+		except Exception as e:
+			result = text + str(pad * amount_to_pad)
+
+		return result
+
+	def __unpad(self, text):
+		pad = ord(text[-1])
+		return text[:-pad]
+
+	def encrypt( self, raw ):
+		raw = self.__pad(raw)
+		cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
+		if(type(raw) == bytes):
+			return cipher.encrypt(raw)
+		else:
+			return cipher.encrypt(raw.encode('utf-8'))
+
+	def decrypt( self, enc ):
+		#enc = base64.b64decode(enc)
+		cipher = AES.new(self.key, AES.MODE_CBC, self.iv )
+		return self.__unpad(cipher.decrypt(enc).decode("utf-8"))
+
+if __name__=="__main__":
+	e = AESCipher('\x00' * 16, '\x00' * 16)
+
+	secret_data = "test"
+	enc_str = e.encrypt(secret_data)
+	print('enc_str: {}'.format(base64.b64encode(enc_str)))
+	dec_str = e.decrypt(enc_str)
+	print('dec str: {}'.format(dec_str))
---
+++ express.js
@@ -0,0 +1,84 @@
+const express=require('express');
+const app=express();
+
+const request = require("request");
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "dlthdus0518",
+  database: "team4",
+});
+
+connection.connect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views',__dirname + '/views');
+app.set('view engine','ejs');
+
+app.get('/index', function(req,res){
+    res.render('index');
+});
+app.get('/login', function(req,res){
+    res.render('login');
+});
+
+app.get('/register', function(req,res){
+    res.render('register');
+});
+
+app.post('/signup', function(req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var bank = req.body.bank;
+    var account = req.body.account;
+
+    var userInsertSql = "INSERT INTO user (`username`, `email`, `password`, `bank`, `account`) VALUES (?, ?, ?, ?, ?);";
+
+    connection.query(userInsertSql, [username, email, password, bank, account], function (error, results, fields) {
+        if (error) throw error;
+        else {
+            res.json(1);
+            res.render('index');
+        }
+    });
+    console.log(req.body);
+});
+
+app.post('/login', function(req, res) {
+    console.log(req.body);
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var userCheckSql = "SELECT * FROM user WHERE email=?"
+    connection.query(userCheckSql, [email], function (error, results, fields) {
+        if (error) throw error;
+        else {
+          if(results.length == 0) {
+              console.log("아이디나 비밀번호가 틀렸습니다.");
+              res.json(2);
+          }
+          else {
+              var storedPassword = results[0].password;
+              if(password == storedPassword) {
+                //로그인 성공
+                res.json(1);
+                console.log("로그인 성공");
+              }
+              else {
+                //로그인 실패
+                res.json(2);
+              }
+          }
+        }
+     });
+});
+
+
+app.listen(3000);
